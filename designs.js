79118554dd511803b203d2971803e1a2a31385b4
raduliviu <baltag.radu@gmail.com>
2017-09-27 17:25:06 +0200

    Added comments to JS file

--- designs.js
+++ designs.js
@@ -1,18 +1,22 @@
+//First I'm preventing the form being submitted by the button
 $(document).ready(function () {
     $('#sizePicker input').last().click(function (event) {
         event.preventDefault();
     });
 });
 
+//Here I'm creating the function that will build our grid
 function makeGrid() {
     $('#pixel_canvas').empty();
     let gridHeight = Number($('#input_height').val());
     let gridWidth = Number($('#input_width').val());
+    //based on the values the user selects, next up the table is being created
     for (let i = 1; i <= gridHeight; i++) {
         let tr = document.createElement('tr');
         for (let z = 1; z <= gridWidth; z++) {
             let td = document.createElement('td');
             tr.appendChild(td);
+            //now I also want not only to change the background color when clicking the cell, I want it to change back when clicking again
             let clicked = false;
             td.onclick = () => {
                 clicked = !clicked;
@@ -23,5 +27,5 @@ function makeGrid() {
         document.querySelector('#pixel_canvas').appendChild(tr);
     }
 };
-
+//Finally, I want to run our function to create the grid when clicking the submit button
 $('#sizePicker input').last().click(makeGrid);
